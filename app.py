565069c5a4f29ebe8ab4c8946b39860c5ebc3207
ruizjosemanuel177-tech <ruizjosemanuel177@gmail.com>
2025-10-08 14:35:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -221,6 +221,8 @@ def exportar_pdf():
     return send_file(pdf_path, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render asigna el puerto automáticamente
+    app.run(host="0.0.0.0", port=port)
+
 
 
